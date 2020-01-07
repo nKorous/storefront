44baec83d3b0eb35051f301861e1ee09ff21d5a7
Nate Korous <nkorous@managerplus.com>
2020-01-07 16:41:21 -0700

    added basic front end

--- front/index.js
+++ front/index.js
@@ -1,1 +1,49 @@
-console.log('hello from the js file')
+const ENDPOINT = 'http://localhost:5200'
+
+function getProductList() {
+    console.log('getting product list')
+    fetch(ENDPOINT + '/productList')
+        .then(res => res.json())
+        .then(data => loadDataIntoTable(data))
+}
+
+function loadDataIntoTable(data) {
+    let t = document.getElementById('tableBody')
+    t.innerHTML = '' //clear out the old data so there is no DOM duplication
+
+    data.map(row => {
+        const { productKey, productName, productDescription, active } = row
+        let tr = document.createElement('tr')
+
+        tr.innerHTML = `
+            <td>${productKey}</td>
+            <td>${productName}</td>
+            <td>${productDescription}</td>
+            <td>${active}</td>
+            `
+
+        t.appendChild(tr)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+getProductList()
